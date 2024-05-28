b84e217c3e40854b6b54f2cdc9ca270c02296e70
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-05-28 14:45:41 +0500

    update

--- Q.13/index.js
+++ Q.13/index.js
@@ -1,10 +1,4 @@
 // Question 13
 // Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-var favoriteTransportation = [
-    'I want to own a Honda civic car.',
-    'Honda Civic is fuel efficient.',
-    'Honda civic looks classy.',
-    'Honda Civic is a reliable car.',
-    'I liKe Honda Civics Safety features.'
-];
-favoriteTransportation.forEach(function (car) { return console.log(car); });
+var favoriteTransportation = ['Honda 125', 'Honda civic', 'Audi R9', 'Bugati '];
+favoriteTransportation.map(function (item) { return (console.log('I would like to own a' + " " + item)); });
--- Q.13/index.ts
+++ Q.13/index.ts
@@ -2,13 +2,8 @@
 
 // Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
-let favoriteTransportation: string[] = [
-'I want to own a Honda civic car.',
-'Honda Civic is fuel efficient.',
-'Honda civic looks classy.',
-'Honda Civic is a reliable car.',
-'I liKe Honda Civics Safety features.'
-];
+let favoriteTransportation: string[] = ['Honda 125', 'Honda civic', 'Audi R9','Bugati '];
 
-favoriteTransportation.forEach(car => console.log(car)
-);
+favoriteTransportation.map((item: string) => (
+    console.log('I would like to own a' + " "+ item )
+))
